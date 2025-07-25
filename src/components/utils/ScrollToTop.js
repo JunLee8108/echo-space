fc5281d0740de6ec0fd91dfd127b4868a0222498
JunLee8108 <lejhn1@gmail.com>
2025-07-25 16:35:56 -0500

    useLayoutEffect added

--- src/components/utils/ScrollToTop.js
+++ src/components/utils/ScrollToTop.js
@@ -1,12 +1,19 @@
-import { useEffect } from "react";
+// ScrollToTop.tsx
+import { useLayoutEffect } from "react";
 import { useLocation } from "react-router";
 
 export default function ScrollToTop() {
-  const { pathname } = useLocation();
+  const { pathname, hash } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+  useLayoutEffect(() => {
+    // 해시(#section) 이동이면 유지하고, 그 외엔 0으로
+    if (!hash) {
+      // Safari 호환: 문서 요소가 없는 경우 대비
+      const scrollTarget =
+        document.scrollingElement || document.documentElement;
+      scrollTarget.scrollTo({ top: 0, left: 0 });
+    }
+  }, [pathname, hash]);
 
-  return null;
+  return null; // 렌더링 없음
 }
