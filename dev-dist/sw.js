82dde0232cc66b3243db1a5a8205d2d8d2a01417
JunLee8108 <lejhn1@gmail.com>
2025-08-05 08:42:59 -0500

    quick fix v4

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-47da91e0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qej66hgm6ds"
+    "revision": "0.aq15g0f6smg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
